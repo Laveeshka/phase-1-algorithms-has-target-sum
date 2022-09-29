366b59590443ba0de96c5429640042609b293921
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-09-30 06:28:48 +1000

    first try with nested loop

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const len = array.length;
+  for (let i = 0; i < len; i++){
+    for (let j = i + 1; j < len; j++){
+      const sum = array[i] + array[j];
+      if (sum === target)
+        return true;
+    }
+  }
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
+  This is the worst case scenario since we end up with  O(n²) complexity arising from the nested loop
 */
 
 /* 
@@ -12,6 +23,13 @@ function hasTargetSum(array, target) {
 
 /*
   Add written explanation of your solution here
+  Use two loops
+  First loop iterates over each element of the array with our trusted index i
+  We have a nested loop (eek! for time and space complexity)
+  The nested loop's index is offset by 1 to that of the first loop
+  Why? - We want to compare the index at the preceeding index with every other element
+  Once their sum matches the target, return true
+  Otherwise, return false
 */
 
 // You can run `node index.js` to view these console logs
